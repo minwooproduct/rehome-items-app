f61d4e2e1b4cc6a972f637d0d7a16c84f7731f32
minwooproduct <minwoo.product@gmail.com>
2024-01-14 15:30:40 +1100

    Installed materialUI. Created a ChipsArray component. This will later become the filter used on the home screen.

---
+++ src/app/testpage/page.js
@@ -0,0 +1,12 @@
+'use client'
+
+import React from 'react'
+import ChipsArray from '../../components/ui/ChipsArray'
+
+export default function TestPage() {
+    return (
+        <div>
+            <ChipsArray/>
+        </div>
+    )
+}
