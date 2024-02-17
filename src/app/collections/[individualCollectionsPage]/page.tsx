428b8073aec1bcb5ac73d434faf7dcab5590054f
minwooproduct <minwoo.product@gmail.com>
2024-02-18 00:20:15 +1100

    Building individual collection page and connecting with database.
    Have now extended the individual collection page so that it retrieves the images from the database based on the uuid of the collection. Included ability to add new images to the collection. Removed pages that were doubled up including uploadForm and backup collection page. Need to work on: how to close the upload popover when an upload is successful + making the add image button sticky + introduce a delete button with confirmation modal.

--- src/app/collections/[individualCollectionsPage]/page.tsx
+++ src/app/collections/[individualCollectionsPage]/page.tsx
@@ -20,6 +20,15 @@ import { useParams } from "next/navigation";
 
 /* COMPONENTS */
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import UploadForm from "@/components/ui/uploadForm";
 
 /* CONNECTIONS */
 import { supabaseClient } from "@/lib/initSupabase";
@@ -70,17 +79,43 @@ async function fetchIndividualCollection(
   };
 }
 
+function cn(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+/* TYPE DEFINITION */
+type Image = {
+  id: number;
+  item_name: string;
+  item_owner: string;
+  item_price: number;
+  imageSrc: string;
+  item_description: string;
+  created_at: string;
+  collection_uuid: string;
+};
+
 export default function Page() {
   /* HOOKS */
   const paramsObjectFromRoute = useParams<{
     individualCollectionsPage: string;
   }>();
-  const [collectionData, setCollectionData] = useState<any[]>([]);
-  const [collectionErrors, setCollectionErrors] =
-    useState<PostgrestError | null>(null);
+  const [refreshCollection, setRefreshCollection] = useState(false);
+  const [collectionImages, setCollectionImages] = useState<any[]>([]);
+  const [collectionErrors, setCollectionErrors] = useState<PostgrestError | null>(null);
+
+  /* ADD IMAGES */
+  const [isUploadPopoverOpen, setUploadPopoverOpen] = useState(false);
+  const togglePopover = () => setUploadPopoverOpen(!isUploadPopoverOpen);
+
+  const closeUploadModalAndResetValues = () => {
+    setRefreshCollection(prev => !prev) //resets the form
+    togglePopover()
+  }
 
   console.log("paramsObject from route is", paramsObjectFromRoute);
 
+  /* On initial mount, fetch the collection based on the URL param*/
   useEffect(() => {
     const initFetch = async () => {
       const { individualCollectionsData, individualCollectionsErrors } =
@@ -88,19 +123,78 @@ export default function Page() {
           supabaseClient,
           paramsObjectFromRoute.individualCollectionsPage
         );
-      setCollectionData(individualCollectionsData);
+      setCollectionImages(individualCollectionsData);
       setCollectionErrors(individualCollectionsErrors);
     };
     initFetch();
-  }, [paramsObjectFromRoute.individualCollectionsPage]);
+  }, [paramsObjectFromRoute.individualCollectionsPage, refreshCollection]);
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="flex justify-center items-center h-screen">
-        {collectionData.map((item) => {
-          return <Button key={item}>{item.item_name}</Button>;
-        })}
+    <div>
+      <div className="mx-auto max-w-2xl pt-2 px-2 sm:pt-6 sm:px-4 lg:max-w-7xl lg:px-4">
+        <div className="mt-2 mb-2 sm:mt-4 sm:mb-4 md:mt-6 md:mb-6 lg:mt-8 lg:mb-8 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {collectionImages?.map((image) => (
+            <BlurImage key={image.id} image={image} />
+          ))}
+        </div>
+      </div>
+      <div className="absolute bottom-0 right-0 mb-4 mr-4 rounded-full h-12 w-12 flex items-center justify-center  text-black  transition duration-150 ease-in-out">
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button onClick={togglePopover}>+</Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-80" >
+            <div>
+              <UploadForm closeUploadModalAndResetValues={closeUploadModalAndResetValues}/>
+            </div>
+          </PopoverContent>
+        </Popover>
       </div>
     </div>
+  );
+}
+
+function BlurImage({ image }: { image: Image }) {
+  const [isLoading, setLoading] = useState(true);
+  const [isHovering, setHovering] = useState(false);
+
+  return (
+    <a href={image.imageSrc} className="group">
+      {/* Tile containing the gallery */}
+      <div
+        className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8"
+        onMouseOver={() => setHovering(true)}
+        onMouseOut={() => setHovering(false)}
+      >
+        {/* Image rendering */}
+        <Image
+          alt=""
+          src={image.imageSrc}
+          layout="fill"
+          objectFit="cover"
+          className={cn(
+            "duration-700 ease-in-out group-hover:opacity-75",
+            isLoading
+              ? "scale-110 blur-2xl grayscale"
+              : "scale-100 blur-0 grayscale-0"
+          )}
+          onLoadingComplete={() => setLoading(false)}
+        />
+
+        {/* When the mouse is hovering over the image, show the description */}
+        {isHovering && image.item_description && (
+          <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white p-4">
+            {image.item_description}
+          </div>
+        )}
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">{image.item_name}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">
+        {image.item_name}
+      </p>
+      <p className="mt-1 text-lg font-medium text-gray-900">
+        {image.item_price}
+      </p>
+    </a>
   );
 }
--- src/components/ui/uploadForm.tsx
+++ src/components/ui/uploadForm.tsx
@@ -2,13 +2,20 @@
 
 "use client";
 
-import { useState, useEffect } from "react";
-
+/* UTILITY */
+import { v4 as uuidv4 } from "uuid";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useParams } from "next/navigation";
+import { useState, useEffect } from "react";
 
-import { useForm } from "react-hook-form";
+/* CONNECTIONS */
+import { supabaseClient } from "@/lib/initSupabase";
+import { useUser } from "@clerk/nextjs";
+import { useSession } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 
+/* COMPONENTS */
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -20,42 +27,62 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-
 import { FilePond, registerPlugin } from "react-filepond";
 import "filepond/dist/filepond.min.css";
 import { FILE_POND_BUCKET } from "@/lib/constant";
-
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
-
-import { supabaseClient } from "@/lib/initSupabase";
-import { useUser } from "@clerk/nextjs";
-import { useSession } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-import { v4 as uuidv4 } from "uuid";
+import { useToast } from "@/components/ui/use-toast";
+import { useForm } from "react-hook-form";
+import LoadingSpinner from "./LoadingSpinner";
 
 const formSchema = z.object({
   item_name: z.string().min(2).max(50),
+  item_owner: z.string().min(2).max(50),
   item_description: z.string().max(250),
   item_price: z.number().int().positive(),
+  imageSrc: z.string(),
+  collection_id: z.string(),
 });
 
-export default function UploadForm() {
+type FormValues = z.infer<typeof formSchema>;
+
+export default function UploadForm({ closeUploadModalAndResetValues }) {
+  /* HOOKS */
   const [files, setFiles] = useState([]);
   const { isSignedIn, user, isLoaded } = useUser();
+  const [isPendingLoading, setPendingLoading] = useState(false);
+
   const router = useRouter();
+  const { toast } = useToast();
 
   const { session } = useSession();
   const token = session?.getToken();
 
+  const paramsObjectFromRoute = useParams<{
+    individualCollectionsPage: string;
+  }>();
+
+  useEffect(() => {
+    if (user) {
+      form.setValue("item_owner", user.id);
+    }
+  }, [user]);
+
   //1. Form definition
-  const form = useForm<z.infer<typeof formSchema>>({
+  const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       item_name: "",
+      item_owner: "",
       item_description: "",
+      item_price: 0,
+      imageSrc: "",
+      collection_id: paramsObjectFromRoute.individualCollectionsPage,
     },
   });
+
+  const { reset, handleSubmit, control, setValue } = form;
 
   //User checks
   if (!isLoaded) {
@@ -66,33 +93,92 @@ export default function UploadForm() {
   }
 
   //2. Submit handler
-  async function onSubmit(values) {
-    if (!files[0]) return; // Make sure there's at least one file selected
-    
+  async function onSubmit(values: FormValues) {
+    setPendingLoading(true);
+
+    // Make sure there's at least one file selected
+    if (!files[0]) {
+      console.log("No files were uploaded");
+      toast({
+        title: "🤔 Hmm... no files detected.",
+        description: `Are you sure you uploaded an image?`,
+        duration: 5000,
+      });
+      return;
+    }
+
     const file = files[0].file; // Assuming you're dealing with the first file
     const uuid = uuidv4(); // Generate a UUID for this file upload
     const filePath = `${user.id}/${uuid}/${file.name}`; // Construct the file path
-  
+
     try {
       // Upload the file to Supabase Storage
-      const { data, error } = await supabaseClient.storage.from("images").upload(filePath, file, {
-        contentType: file.type, // Optionally set the MIME type (contentType) of the file
-        upsert: false // Example option: if set to true, will overwrite existing files with the same path
-      });
-  
+      const { data, error } = await supabaseClient.storage
+        .from("images")
+        .upload(filePath, file, {
+          contentType: file.type, // Optionally set the MIME type (contentType) of the file
+          upsert: false, // Example option: if set to true, will overwrite existing files with the same path
+        });
+
       if (error) throw error;
-  
+
       // Success - handle the response, e.g., storing the file URL in your database
-      console.log('File uploaded successfully:', data);
+      console.log("File uploaded successfully:", data);
+
+      const imageUrl = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/${filePath}`;
+
+      form.setValue("imageSrc", imageUrl);
+      const { error: insertImageError } = await supabaseClient
+        .from("images")
+        .insert({
+          item_name: values.item_name,
+          item_owner: user.id,
+          item_description: values.item_description,
+          item_price: values.item_price,
+          imageSrc: imageUrl,
+          collection_id: paramsObjectFromRoute.individualCollectionsPage,
+        });
+
+      if (insertImageError) {
+        console.log(`Image failed to upload with error ${insertImageError}`);
+        toast({
+          title: "😭 There was an issue uploading your image",
+          description: `Error: ${insertImageError}`,
+          duration: 5000,
+        });
+      } else {
+        console.log("Image successfully uploaded");
+        toast({
+          title: "🥳 Congrats! Image uploaded!",
+          description: `You'll soon see the image in your collection`,
+          duration: 5000,
+        });
+        closeUploadModalAndResetValues(); //Re-render the parent page
+
+        
+        
+      }
+      setPendingLoading(false);
+      reset()
+      setFiles([]);
     } catch (error) {
-      console.error('Upload error:', error.message);
+      console.error("Upload error:", error.message);
+      toast({
+        title: "Error uploading file",
+        description: "An unexpected error occurred. Please try again.",
+        duration: 5000,
+      });
+      setPendingLoading(false);
     }
   }
 
   return (
-    <div className="flex justify-center items-center w-full h-screen">
+    <div className="">
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <form
+          onSubmit={form.handleSubmit(onSubmit, onError)}
+          className="space-y-8"
+        >
           <FormField
             control={form.control}
             name="item_name"
@@ -158,14 +244,21 @@ export default function UploadForm() {
               onupdatefiles={setFiles}
               acceptedFileTypes={["image/*"]}
               allowMultiple={false}
-              maxfiFiles={1}
+              maxFiles={1}
               name="files"
-              labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+              labelIdle='Drag and drop images or <span class="filepond--label-action">Browse</span>'
             />
           </div>
-          <Button type="submit">Upload</Button>
+          <button type="submit" disabled={isPendingLoading}>
+            {isPendingLoading ? "Uploading..." : "Upload"}
+          </button>
         </form>
       </Form>
+      {isPendingLoading && <LoadingSpinner />}
     </div>
   );
 }
+
+const onError = (errors) => {
+  console.error("Form errors:", errors);
+};
